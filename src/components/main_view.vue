519f1534bc8ed5c581ccb40d622f7a8250514988
wangqi <1722009706@qq.com>
2023-08-28 15:22:29 +0800

    ring_icon_button 0.2

--- src/components/main_view.vue
+++ src/components/main_view.vue
@@ -2,10 +2,10 @@
     <div class="child_container">
         <Navigation></Navigation>
         <div class="model_btn">
-            <RingIconButton title="全景"></RingIconButton>
-            <RingIconButton title="发射机"></RingIconButton>
-            <RingIconButton title="机柜"></RingIconButton>
-            <RingIconButton title="电源"></RingIconButton>
+            <RingIconButton title="全景" bid="all" :flag="flagOne" @click="handleOne"></RingIconButton>
+            <RingIconButton title="发射机" bid="one" :flag="flagTwo" @click="handleTwo"></RingIconButton>
+            <RingIconButton title="机柜" bid="two" :flag="flagThree" @click="handleThree"></RingIconButton>
+            <RingIconButton title="电源" bid="three" :flag="flagFour" @click="handleFour"></RingIconButton>
         </div>
     </div>
 </template>
@@ -16,6 +16,36 @@ import RingIconButton from "./module/ring_icon_button.vue"
 import { ref, onMounted, getCurrentInstance } from "vue";
 
 const { proxy } = getCurrentInstance();
+
+const flagOne = ref(true)
+const flagTwo = ref(false)
+const flagThree = ref(false)
+const flagFour = ref(false)
+
+const handleOne = () => {
+    flagOne.value = true
+    flagTwo.value = false
+    flagThree.value = false
+    flagFour.value = false
+}
+const handleTwo = () => {
+    flagOne.value = false
+    flagTwo.value = true
+    flagThree.value = false
+    flagFour.value = false
+}
+const handleThree = () => {
+    flagOne.value = false
+    flagTwo.value = false
+    flagThree.value = true
+    flagFour.value = false
+}
+const handleFour = () => {
+    flagOne.value = false
+    flagTwo.value = false
+    flagThree.value = false
+    flagFour.value = true
+}
 
 </script>
 
--- src/components/module/ring_icon_button.vue
+++ src/components/module/ring_icon_button.vue
@@ -1,21 +1,30 @@
 <template>
-    <div class="ring_icon_button">
+    <!-- [{ active: isActive }, errorClass] -->
+    <div class="ring_icon_button" :class="[props.bid, { suc: props.flag }]">
         <div class="ring_btn_out">
             <div class="ring_btn_middle">
                 <div class="ring_btn_in">
-                    <div class="ring_btn_icon"></div>
+                    <img class="ring_btn_icon" :src="props.flag ? './icon/icon01.svg' : './icon/icon02.svg'" />
                 </div>
             </div>
         </div>
         <div class="title">{{ title }}</div>
-        <GradientLine></GradientLine>
+        <GradientLine v-show="props.flag"></GradientLine>
     </div>
 </template>
 
 <script setup>
 import GradientLine from "./gradient_line.vue"
-import { ref } from "vue";
-defineProps(['title'])
+import { ref, onMounted } from "vue";
+
+const props = defineProps({
+    flag: Boolean,
+    bid: String,
+    title: String
+})
+
+onMounted(() => {
+})
 </script>
 
 <style scoped lang="scss">
@@ -30,22 +39,21 @@ defineProps(['title'])
         width: 100%;
         height: calc(100% - 40px);
         margin: 5px 0;
-        border: 1px solid rgba(167, 182, 51, 0.7);
-        box-shadow: 0 0 1px rgba(141, 155, 34, 0.7),
-            0 0 2px rgba(141, 155, 34, 0.7),
-            inset 0 0 1px rgba(141, 155, 34, 0.7),
-            inset 0 0 2px rgba(141, 155, 34, 0.7);
+        border: 1px solid rgba(125, 151, 228, 1);
+        box-shadow: 0 0 1px rgba(39, 72, 169, 0.8),
+            0 0 2px rgba(39, 72, 169, 0.8),
+            inset 0 0 1px rgba(39, 72, 169, 0.8),
+            inset 0 0 2px rgba(39, 72, 169, 0.8);
         border-radius: 50%;
 
         .ring_btn_middle {
-            width: 76%;
-            height: 76%;
-            border: 6px solid rgba(226, 255, 128, 0.9);
-            ;
-            box-shadow: 0 0 2px rgba(221, 255, 110, 0.7),
-                0 0 4px rgba(221, 255, 110, 0.7),
-                inset 0 0 2px rgba(221, 255, 110, 0.7),
-                inset 0 0 4px rgba(221, 255, 110, 0.7);
+            width: 74%;
+            height: 74%;
+            border: 6px solid rgba(130, 172, 237, 1);
+            box-shadow: 0 0 2px rgba(130, 172, 237, 0.8),
+                0 0 4px rgba(130, 172, 237, 0.8),
+                inset 0 0 2px rgba(130, 172, 237, 0.8),
+                inset 0 0 4px rgba(130, 172, 237, 0.8);
             border-radius: 50%;
             position: relative;
             top: 50%;
@@ -53,13 +61,13 @@ defineProps(['title'])
             transform: translate(-50%, -50%);
 
             .ring_btn_in {
-                width: 84%;
-                height: 84%;
-                border: 1px solid rgba(189, 209, 133, 1);
-                box-shadow: 0 0 1px rgba(189, 209, 133, 0.7),
-                    0 0 2px rgba(189, 209, 133, 0.7),
-                    inset 0 0 1px rgba(189, 209, 133, 0.7),
-                    inset 0 0 2px rgba(189, 209, 133, 0.7);
+                width: 80%;
+                height: 80%;
+                border: 2px solid rgba(101, 121, 187, 1);
+                box-shadow: 0 0 1px rgba(52, 75, 154, 0.8),
+                    0 0 2px rgba(52, 75, 154, 0.8),
+                    inset 0 0 2px rgba(80, 107, 196, 0.8),
+                    inset 0 0 4px rgba(80, 107, 196, 0.8);
                 border-radius: 50%;
                 position: relative;
                 top: 50%;
@@ -69,7 +77,7 @@ defineProps(['title'])
                 .ring_btn_icon {
                     width: 65%;
                     height: 65%;
-                    background-color: antiquewhite;
+                    // background-color: antiquewhite;
                     position: relative;
                     top: 50%;
                     left: 50%;
@@ -88,6 +96,36 @@ defineProps(['title'])
         font-weight: bold;
         color: #ffffff;
         text-align: center;
+    }
+}
+
+.ring_icon_button.suc {
+    .ring_btn_out {
+        border: 1px solid rgba(167, 182, 51, 1);
+        box-shadow: 0 0 1px rgba(141, 155, 34, 0.8),
+            0 0 2px rgba(141, 155, 34, 0.8),
+            inset 0 0 1px rgba(141, 155, 34, 0.8),
+            inset 0 0 2px rgba(141, 155, 34, 0.8);
+
+        .ring_btn_middle {
+            border: 6px solid rgba(226, 255, 128, 1);
+            box-shadow: 0 0 2px rgba(221, 255, 110, 0.8),
+                0 0 4px rgba(221, 255, 110, 0.8),
+                inset 0 0 2px rgba(221, 255, 110, 0.8),
+                inset 0 0 4px rgba(221, 255, 110, 0.8);
+
+            .ring_btn_in {
+                border: 1px solid rgba(189, 209, 133, 1);
+                box-shadow: 0 0 1px rgba(189, 209, 133, 0.8),
+                    0 0 2px rgba(189, 209, 133, 0.8),
+                    inset 0 0 2px rgba(189, 209, 133, 0.8),
+                    inset 0 0 4px rgba(189, 209, 133, 0.8);
+            }
+        }
+    }
+
+    .title {
+        color: rgba(230, 255, 147, 1);
     }
 }
 </style>
